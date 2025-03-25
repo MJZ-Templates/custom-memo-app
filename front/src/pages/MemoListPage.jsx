2057193840b70bf66dbce80b6084a98f6d6e5573
misung-dev <misung.dev@gmail.com>
2025-03-25 17:14:28 +0900

    remove: 메모 목록 페이지 내 불필요한 모달 코드 삭제

--- front/src/pages/MemoListPage.jsx
+++ front/src/pages/MemoListPage.jsx
@@ -140,33 +140,3 @@ const MemoListContainer = styled.div`
   margin: 0 auto;
   width: 100%;
 `;
-
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-`;
-
-const ModalContent = styled.div`
-  background: white;
-  padding: 20px;
-  border-radius: 10px;
-  position: relative;
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  background: none;
-  border: none;
-  font-size: 20px;
-  cursor: pointer;
-`;
